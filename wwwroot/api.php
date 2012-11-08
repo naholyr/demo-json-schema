c695c47383f1939df1ec015312912aa8bb1ae3fb
Nicolas Chambrier <naholyr@gmail.com>
2012-11-08 17:21:58 +0100

    Books DB & API

--- wwwroot/api.php
+++ wwwroot/api.php
@@ -11,6 +11,49 @@ use \Exception;
 
 $app = new Silex\Application();
 
+
+// Books lolilol database
+
+$data_file = __DIR__ . '/data/books.data.php';
+if (!is_file($data_file)) {
+  copy($data_file . '-dist', $data_file);
+}
+
+$app['books_db'] = array(
+  'data' => require $data_file,
+  'save' => function () use ($app, $data_file) {
+    file_put_contents($data_file, $app['books_db']['data']);
+  }
+);
+
+
+// "books" API
+
+$app->get('/books', function () use ($app) {
+  return $app->json($app['books_db']['data']);
+});
+
+$app->post('/books', $checkJSON, function () use ($app) {
+  // TODO
+});
+
+$app->get('/books/{id}', function () use ($app) {
+  // TODO
+});
+
+$app->put('/books/{id}', function () use ($app) {
+  // TODO
+});
+
+$app->delete('/books/{id}', function () use ($app) {
+  // TODO
+});
+
+$app->delete('/books', function () use ($app) {
+  // TODO
+});
+
+
 // Start server (kinda)
 
 $app->run();
