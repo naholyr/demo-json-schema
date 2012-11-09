41c46453f45dca32fc8702ff29b0849627e08b4a
Nicolas Chambrier <naholyr@gmail.com>
2012-11-09 10:49:58 +0100

    Load books list

--- wwwroot/books.js
+++ wwwroot/books.js
@@ -1,1 +1,29 @@
-// TODO
+jQuery(function ($) {
+
+  // Load list
+
+  listBooks = (function () {
+    var $list = $('#books-list');
+    var $waiting = $('#books-list-waiting');
+    var tpl = $('#book-template').text();
+    return function doRefreshListBooks () {
+      $.ajax({ url: "/books", method: "GET" })
+      .fail(function (err) { alert('OH FUCK !\n' + err); })
+      .done(function (books) {
+        console.log('Books list', books);
+        // Yeah, modern browsers only, it's a demo man, live with it
+        var html = books.map(function (book) {
+          return tpl
+            .replace(/\{\{title\}\}/g, book.title)
+            .replace(/\{\{price\}\}/g, book.price)
+            .replace(/\{\{authors\}\}/g, book.authors.map(function (author) { return author.name }).join(', '))
+        });
+        $list.append($(html.join('')));
+        $waiting.hide();
+        $list.show();
+      })
+    };
+  })();
+
+  listBooks();
+});
